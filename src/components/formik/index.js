ba1969139cbf9f5b7de38fd025ce58d3a4535947
pshadikbasha <pshadikbasha97@gmail.com>
2021-08-06 11:22:55 +0530

    Adding initial vlaues in useFormiK hook

--- src/components/formik/index.js
+++ src/components/formik/index.js
@@ -2,16 +2,41 @@ import "./style.css";
 import { useFormik } from "formik";
 
 const SignUp = () => {
-  const formIk = useFormik({});
+  const formIk = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+  console.log("formIk values", formIk.values);
   return (
     <div>
       <form action="">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={formIk.handleChange}
+          value={formIk.values.name}
+        />
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={formIk.handleChange}
+          value={formIk.values.email}
+        />
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+        <input
+          type="text"
+          name="channel"
+          id="channel"
+          onChange={formIk.handleChange}
+          value={formIk.values.channel}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
